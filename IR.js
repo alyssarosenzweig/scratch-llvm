1d87392a90b8eef1ecd2ff17abb7ed8caf117abc
bobbybee <j104@rocketmail.com>
2014-10-22 18:54:22 -0400

    Further processing

--- IR.js
+++ IR.js
@@ -13,6 +13,19 @@ function parse(file) {
 		if(regexs.define.test(lines[i])) {
 			var m = lines[i].match(regexs.define);
 			console.log(m);
+
+			var returnType = m[1];
+			var funcName = m[2];
+			var paramList = m[3];
+			var modifiers = m[4];
+
+			var params = paramList.split(',');
+			var formattedParams = [];
+			for(var j = 0; j < params.length; ++j) {
+				formattedParams.push( params[j].trim().split(' ') );
+			}
+
+			console.log(returnType+" "+funcName+"("+formattedParams+");");
 		}
 	}
 }
