54d4198d2540962bc1379bbe52beefb670f562ef
bobbybee <j104@rocketmail.com>
2014-10-22 18:58:30 -0400

    Proper function definitions

--- IR.js
+++ IR.js
@@ -9,25 +9,46 @@ var regexs = {
 function parse(file) {
 	var lines = file.split('\n');
 
+	var mod = {
+		functions: []
+	};
+
+	var inFunctionBlock = false;
+	var functionBlock = {};
+
 	for(var i = 0; i < lines.length; ++i) {
-		if(regexs.define.test(lines[i])) {
-			var m = lines[i].match(regexs.define);
-			console.log(m);
+		if(!inFunctionBlock) {
+			if(regexs.define.test(lines[i])) {
+				var m = lines[i].match(regexs.define);
 
-			var returnType = m[1];
-			var funcName = m[2];
-			var paramList = m[3];
-			var modifiers = m[4];
+				var returnType = m[1];
+				var funcName = m[2];
+				var paramList = m[3];
+				var modifiers = m[4];
 
-			var params = paramList.split(',');
-			var formattedParams = [];
-			for(var j = 0; j < params.length; ++j) {
-				formattedParams.push( params[j].trim().split(' ') );
+				var params = paramList.split(',');
+				var formattedParams = [];
+				for(var j = 0; j < params.length; ++j) {
+					formattedParams.push( params[j].trim().split(' ') );
+				}
+
+				functionBlock = {
+					returnType: returnType,
+					paramList: formattedParams,
+					funcName: funcName,
+					code: []
+				};
+				inFunctionBlock = true;
 			}
-
-			console.log(returnType+" "+funcName+"("+formattedParams+");");
+		} else {
+			if(lines[i] == "}") {
+				mod.functions.push(functionBlock);
+				inFunctionBlock = false;
+			}
 		}
 	}
+
+	return mod;
 }
 
 module.exports = function(filename) {
