d07b5c651a8d22d7bfacb1356e50758bc0273da1
bobbybee <j104@rocketmail.com>
2014-10-22 18:19:03 -0400

    Hello, World test files

---
+++ helloworld.s
@@ -0,0 +1,12 @@
+; ModuleID = 'tests/helloworld.c'
+target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.9.0"
+
+@.str = private unnamed_addr constant [14 x i8] c"Hello, World!\00", align 1
+
+define void @main() nounwind ssp uwtable {
+  call void @puts(i8* getelementptr inbounds ([14 x i8]* @.str, i32 0, i32 0))
+  ret void
+}
+
+declare void @puts(i8*)
---
+++ tests/helloworld.c
@@ -0,0 +1,5 @@
+void puts(char*);
+
+void main() {
+	puts("Hello, World!");
+}
