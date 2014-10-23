14fc9105016aa8ff5ebd016f3b3ca031af77c54f
bobbybee <j104@rocketmail.com>
2014-10-22 20:04:14 -0400

    Generate custom block hats for functions

--- backend.js
+++ backend.js
@@ -1,6 +1,39 @@
 // takes an IR function object and returns a list of Scratch blocks
 
-module.export.compileFunction = function(func) {
-	console.log("Compiling "+func+"...");
-	return [];
+module.exports.compileFunction = function(func) {
+	console.log("Compiling "+JSON.stringify(func)+"...");
+
+	var blockList = [];
+
+	// add a procdef
+
+	var spec = func.funcName;
+	var inputs = [];
+	var defaults = [];
+
+	for(var i = 0; i < func.paramList.length; ++i) {
+		if(func.paramList[i][1])
+			inputs.push(func.paramList[i][1]);
+		else
+			inputs.push("param"+i);
+		
+		defaults.push(defaultForType(func.paramList[i][0]));
+		spec += " "+specifierForType(func.paramList[i][0]);
+	}
+
+
+	blockList.push(["procDef", spec, inputs, defaults, false]);
+
+	return blockList;
+}
+
+// fixme: stub
+function defaultForType(type) {
+	console.log(type);
+	return 0;
+}
+
+// fixme: stub
+function specifierForType(type) {
+	return "%s";
 }
