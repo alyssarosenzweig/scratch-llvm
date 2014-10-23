0c6b5b422a690451518067ba2246868d286bb007
bobbybee <j104@rocketmail.com>
2014-10-23 15:21:44 -0400

    Generate whenGreenFlag to call @main

--- meowcc.js
+++ meowcc.js
@@ -22,6 +22,11 @@ for(var i = 0; i < IR.functions.length; ++i) {
 	meow.addScript(backend.compileFunction(IR.functions[i]));
 }
 
+meow.addScript([
+		["whenGreenFlag"],
+		["call", "@main"] // TODO: argc + argv
+	])
+
 if(process.argv[3]) {
 	meow.upload(process.argv[3], 'v426', process.argv[4], process.argv[5]);
 } else {
