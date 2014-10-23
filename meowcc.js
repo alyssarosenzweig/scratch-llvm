8526b55d2bf96e031f374a41cd1da890a77e21e9
bobbybee <j104@rocketmail.com>
2014-10-23 07:12:58 -0400

    Allow for offline testing of meowcc

--- meowcc.js
+++ meowcc.js
@@ -10,4 +10,8 @@ for(var i = 0; i < IR.functions.length; ++i) {
 	meow.addScript(backend.compileFunction(IR.functions[i]));
 }
 
-meow.upload(process.argv[3], 'v426', process.argv[4], process.argv[5]);
+if(process.argv[3]) {
+	meow.upload(process.argv[3], 'v426', process.argv[4], process.argv[5]);
+} else {
+	console.log(JSON.stringify(meow.serialize()));
+}
