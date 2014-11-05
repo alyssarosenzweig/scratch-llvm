aa383a1ba93c54fb0ff30ab847244e8b79541f15
bobbybee <j104@rocketmail.com>
2014-11-05 07:53:54 -0500

    Integer size computation

--- backend.js
+++ backend.js
@@ -383,11 +383,19 @@ function LLVMType(str) {
 		this.typeSize = this.arraySize * getTypeSize(this.elementType);
 	} else if(/i\d+/.test(str)) {
 		this.isInteger = true;
-		this.integerBits = str.slice(1);
+		this.integerBits = str.slice(1) * 1;
 		this.typeSize = getIntegerSize(this.integerBits);
 	}
 }
 
 function getTypeSize(type) {
 	return type.typeSize;
+}
+
+function getIntegerSize(size) {
+	return (size == 1 || size == 8) ? 1 : 
+			(size == 16) ? 2 :
+			(size == 32) ? 4 :
+			(size == 64) ? 8 :
+			1;
 }
