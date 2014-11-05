f2de389af8011bcd4122f64b7805405eb3cd4da3
bobbybee <j104@rocketmail.com>
2014-11-05 08:17:51 -0500

    LLVMType bugfixes from nested arrays

--- LLVMType.js
+++ LLVMType.js
@@ -1,13 +1,15 @@
 function LLVMType(str) {
 	if(str[0] == "[") {
-		str = str.slice(1, -1).split(" x ");
+		bareStr = str.slice(1, -1);
+		str = bareStr.split(" x ");
 
 		this.isArray = true;
 		this.arraySize = str[0] * 1;
-		this.elementType = new LLVMType(str[1]);
+		this.elementType = new LLVMType(bareStr.slice(str[0].length + 3));
 		this.typeSize = this.arraySize * getTypeSize(this.elementType);
 	} else if(/i\d+/.test(str)) {
 		this.isInteger = true;
+		console.log(str);
 		this.integerBits = str.slice(1) * 1;
 		this.typeSize = getIntegerSize(this.integerBits);
 	}
