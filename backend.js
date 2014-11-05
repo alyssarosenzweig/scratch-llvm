85f1931a333676a94ebb2e3643b79b782c0b828f
bobbybee <j104@rocketmail.com>
2014-11-05 07:56:47 -0500

    Move LLVMType into its own file as it is shared code

--- backend.js
+++ backend.js
@@ -372,30 +372,3 @@ function icmpBlock(ctx, block) {
 
 	return castToNumber(b);
 }
-
-function LLVMType(str) {
-	if(str[0] == "[") {
-		str = str.slice(1, -1).split(" x ");
-
-		this.isArray = true;
-		this.arraySize = str[0] * 1;
-		this.elementType = new LLVMType(str[1]);
-		this.typeSize = this.arraySize * getTypeSize(this.elementType);
-	} else if(/i\d+/.test(str)) {
-		this.isInteger = true;
-		this.integerBits = str.slice(1) * 1;
-		this.typeSize = getIntegerSize(this.integerBits);
-	}
-}
-
-function getTypeSize(type) {
-	return type.typeSize;
-}
-
-function getIntegerSize(size) {
-	return (size == 1 || size == 8) ? 1 : 
-			(size == 16) ? 2 :
-			(size == 32) ? 4 :
-			(size == 64) ? 8 :
-			1;
-}
