0446bec6b9a3476ab9e26e0470fa4d7b415c39a8
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-22 13:43:56 -0400

    Small bugfix on fizzbuzz example

--- tests/fizzbuzz.c
+++ tests/fizzbuzz.c
@@ -4,12 +4,13 @@ void puts(char*);
 void putnum(int num) {
   putchar( '0' + ((num - (num % 10)) / 10) );
   putchar( '0' + (num % 10));
+  putchar('\n');
 }
 
 void main() {
-  for(int i = 0; i < 100; ++i) {
+  for(int i = 1; i < 100; ++i) {
     if( (i % 15) == 0) {
-      puts("Fizzbuzz\n");
+      puts("Fizzbuzz");
     } else if( (i % 3) == 0) {
       puts("Fizz\n");
     } else if( (i % 5) == 0) {
